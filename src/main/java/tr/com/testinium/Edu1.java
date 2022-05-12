557fc3ae362d1ee60ab16bd45af06e2e5e99f9ee
ebubekir <ebubekir.durukal@testinium.com>
2022-05-12 14:28:48 +0300

    Development yapıldı

--- src/main/java/tr/com/testinium/Edu1.java
+++
@@ -1,8 +0,0 @@
-package tr.com.testinium;
-
-public class Edu1 {
-    public static void main(String[] args) {
-
-        System.out.println("Development");
-    }
-}
---
+++ src/main/java/tr/com/testinium/EduOne.java
@@ -0,0 +1,9 @@
+package tr.com.testinium;
+
+public class EduOne {
+
+    public static void main(String[] args) {
+        System.out.println("Merhaba Testinium");
+    }
+
+}
