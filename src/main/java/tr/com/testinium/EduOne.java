486cc732375d770a7d25183124d0a808db0464e0
ebubekir <ebubekir.durukal@testinium.com>
2022-05-12 14:51:36 +0300

    yorum satırları dersi tamamlandı

--- src/main/java/tr/com/testinium/EduOne.java
+++ src/main/java/tr/com/testinium/EduOne.java
@@ -2,6 +2,23 @@ package tr.com.testinium;
 
 public class EduOne {
 
+    final static double E_SAYISI = 2.71;
+    int normalBirSayi;
+    // burada yanlış örneklere başlıyorum.
+    // int 5normal;
+    // int *asf;
+
+    /*
+    burası multiline
+    bir yorum örneği
+    main metod
+     */
+
+    /**
+     * @author Ebubekir Durukal
+     * @date 12/05/22
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Merhaba Testinium");
     }
